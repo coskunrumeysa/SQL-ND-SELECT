7eb53f0e76b608ea9de3d917cfb2e0aa9bd75c50
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2023-02-21 12:22:12 +0300

    Add files via upload

---
+++ ND_Select.sql
@@ -0,0 +1,7 @@
+
+
+select * from [NORTHWND].[dbo].[Customers];
+--This code written for get all records of Northwnd database dbo schema and customers table.
+
+select * from dbo.Customers;
+--This code written for get all records of Northwnd database dbo schema and customers table.
